7cab2474d4a5ac964c44d8fc1093cdd0e004bfc9
Umang Bhalla <umg.bhalla88@gmail.com>
2021-08-13 21:51:32 +0530

    added PS0

---
+++ Assignments_src/6.0001/PS0/ps0.py
@@ -0,0 +1,18 @@
+# |\     /|     @author: umgbhalla
+# | )   ( |     date: 13 Aug 2021
+# | |   | |     course: MIT OCW 6.0001
+# | |   | |
+# | |   | |
+# | (___) |
+# (_______)
+
+import numpy
+
+def main():
+    x = int(input("Enter a number X: "))
+    y = int(input("Enter an other number Y: "))
+    print("X**Y =", x**y , "\n"+ "log(x) =" , numpy.log2(x))
+
+
+if __name__ == "__main__":
+    main()
